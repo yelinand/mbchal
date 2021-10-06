45b428059e2a37d13987db96b2cfe930511b02e6
yelinan <selenith@gmail.com>
2021-10-06 21:38:55 +0200

    fixes

--- mbchal_async.py
+++ mbchal_async.py
@@ -8,7 +8,6 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 rrurl = 'https://reqres.in/api/users'
 results_per_page = 3
 
-#to check for pagination
 def make_request_url(results_per_page, current_page_number):
     return (
         "https://reqres.in/api/users"
@@ -47,37 +46,12 @@ async def gather_data(url_list):
         results = await asyncio.gather(*tasks)
         return results
 
-# def get_result(current_page_number):
-#     with requests.session() as s:
-#         url = make_request_url(results_per_page, current_page_number)
-#         resp = s.get(url, verify=False)
-
-#     if resp.status_code != 200:
-#         raise Exception(resp.status_code)
-
-#     raw = resp.json() 
-#     data = raw.get("data")  #list
-
-#     if (raw["page"] == raw["total_pages"]):
-#          return data
-
-    # return data + get_result(current_page_number + 1)  # concat lists
-
-# def puttocsv(datablock):
-#     with open("output.csv","w",newline="") as f:  
-#         title = "id,email,first_name,last_name,avatar".split(",") # quick hack
-#         cw = csv.DictWriter(f,title,delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#         cw.writeheader()
-#         cw.writerows(datablock)
-
-
 def pandastohtml(datablock):
     df = pandas.DataFrame(datablock)
     with open('mbchal.html', 'w') as f:
         df.to_html(buf=f,escape=False,index=False)
     
-def main():
-    
+def main():    
     total_pages = grab_total_pages(results_per_page)
     url_list = make_url_list(results_per_page,1,total_pages)
     datablock = asyncio.run(gather_data(url_list=set(url_list)))
@@ -86,10 +60,6 @@ def main():
         block['avatar'] = '<img src="' + block['avatar'] + '" width="60" >'
     flat_datablock.sort(key=lambda s: s['id'])
     pandastohtml(flat_datablock)
- 
-    # Then you can print the data or save it to a file
 
 if __name__ == "__main__":
-    # Now run the driver function
-    datablock = [] 
     main()
